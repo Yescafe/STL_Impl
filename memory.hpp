27d22da0b5542f2ff6fbb7aaca26a44e806e18a3
Ivan Chien <qyc027@gmail.com>
2020-05-08 02:27:51 +0800

    Add `memory` part
    - Add memory.hpp
    - Add memory/alloc.hpp
    - Add memory/utils.hpp (not finished)

---
+++ memory.hpp
@@ -0,0 +1,11 @@
+#ifndef STL_IMPL_MEMORY_
+#define STL_IMPL_MEMORY_
+
+#include "memory/alloc.hpp"
+#include "memory/utils.hpp"
+
+namespace stl {
+    using namespace memory;
+}
+
+#endif /* STL_IMPL_MEMORY */
