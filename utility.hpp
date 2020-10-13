bd629c780e259e2e5216e272af5d90e31dc72f76
yescafe <qyc027@gmail.com>
2020-10-14 00:44:20 +0800

    Add `make_pair`

--- utility.hpp
+++ utility.hpp
@@ -41,6 +41,11 @@ struct pair {
     }
 };
 
+template<typename T1, typename T2>
+pair<T1, T2> make_pair(const T1& t1, const T2& t2) {
+    return pair<T1, T2>(t1, t2);
+}
+
 auto rand() -> decltype(std::rand()) {
     static auto initialized = false;
     if (!initialized) {
